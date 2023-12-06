c9f7e6b88e8e2e438362a921fe2ca7df26132284
CMDRZero <gbell12907@gmail.com>
2023-12-05 22:30:49 -0600

    Add Chaser
    I did it, I added them, Dashers prolly next or smth

--- Assets/Scripts/Spawnables/Chasers/ChaserBehavior.cs
+++ Assets/Scripts/Spawnables/Chasers/ChaserBehavior.cs
@@ -9,13 +9,17 @@ public class ChaserBehavior : MonoBehaviour
     public float attackDist;
     public float accel;
     public float maxSpeed;
+    public float movVal;
 
     private Vector2 _movDir;
+    private Vector2 _oldMovDir;
     private float _currSpeed;
+    private GunHandler _gun;
 
     private void Start()
     {
         if (target == null) target = GameObject.FindGameObjectWithTag("Player");
+        _gun = GetComponent<GunHandler>();
     }
 
     private void Update()
@@ -23,16 +27,23 @@ public class ChaserBehavior : MonoBehaviour
         Vector2 diff = (target.transform.position - transform.position);
         if (diff.sqrMagnitude < attackDist * attackDist)
         {
-            //attack
+            _currSpeed *= Mathf.Pow(.9f, Time.deltaTime);
+            Vector2 tarmovDir = UtilFuncs.LeadShotNorm(diff, target.GetComponent<Rigidbody2D>().velocity - GetComponent<Rigidbody2D>().velocity, _gun.ExpectedVelocity());
+            _movDir = UtilFuncs.LerpSafe(_movDir, tarmovDir, 10 * Time.deltaTime);
+            _gun.Shoot(0);
+            //Debug.Log(_gun.status);
         } else
         {
-            float halfDistV = diff.magnitude / 2;
+            float halfDistV = diff.magnitude * movVal;
             Vector2 e1 = (Vector2)transform.position + halfDistV * GetComponent<Rigidbody2D>().velocity.normalized;
             Vector2 e2 = (Vector2)target.transform.position - halfDistV * target.GetComponent<Rigidbody2D>().velocity.normalized;
             Vector2 em = (e1 + e2) / 2;
-            _movDir = pathfinder.PathDirNorm(transform.position, em);
+
+            Vector2 tarmovDir = pathfinder.PathDirNorm(transform.position, em);
+            _movDir = UtilFuncs.LerpSafe(_movDir, tarmovDir, 10 * Time.deltaTime);
             _currSpeed = Mathf.Min(maxSpeed, _currSpeed + accel * Time.deltaTime);
-            GetComponent<Rigidbody2D>().velocity = _movDir * _currSpeed;
         }
+        GetComponent<Rigidbody2D>().velocity = _movDir * _currSpeed;
+        transform.rotation = UtilFuncs.RotFromNorm(_movDir);
     }
 }
---
+++ Assets/Scripts/Util/UtilFuncs.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UtilFuncs
+{
+    public static Vector2 LeadShotNorm(Vector2 relPos, Vector2 relVel, float bulletVel)
+    {
+        float a = bulletVel * bulletVel - relVel.sqrMagnitude;
+        float b = 2 * Vector2.Dot(relPos, relVel);
+        float c = relPos.sqrMagnitude;
+
+        if (b * b + 4 * a * c < 0 || a == 0)
+        {
+            return Vector2.zero;
+        }
+
+        float colTime = (b + Mathf.Sqrt(b * b + 4 * a * c)) / (2 * a);
+        Vector2 colPos = relPos + colTime * relVel;
+        return colPos.normalized;
+    }
+    public static float LeadShot(Vector2 relPos, Vector2 relVel, float bulletVel)
+    {
+        float a = bulletVel * bulletVel - relVel.sqrMagnitude;
+        float b = 2 * Vector2.Dot(relPos, relVel);
+        float c = relPos.sqrMagnitude;
+
+        if (b * b + 4 * a * c < 0 || a == 0)
+        {
+            return 0;
+        }
+
+        float colTime = (b + Mathf.Sqrt(b * b + 4 * a * c)) / (2 * a);
+        Vector2 colPos = relPos + colTime * relVel;
+        return Mathf.Atan2(colPos.y, colPos.x);
+    }
+    public static Quaternion RotFromNorm(Vector2 vec)
+    {
+        return Quaternion.Euler(0, 0, -90 + Mathf.Rad2Deg * Mathf.Atan2(vec.y, vec.x));
+    }
+
+    public static float LerpSafe(float a, float b, float t)
+    {
+        if (t < 0) return a;
+        if (t > 1) return b;
+        return Lerp(a, b, t);
+    }
+    public static Vector2 LerpSafe(Vector2 a, Vector2 b, float t)
+    {
+        if (t < 0) return a;
+        if (t > 1) return b;
+        return Lerp(a, b, t);
+    }
+    public static Vector3 LerpSafe(Vector3 a, Vector3 b, float t)
+    {
+        if (t < 0) return a;
+        if (t > 1) return b;
+        return Lerp(a, b, t);
+    }
+    public static float Lerp(float a, float b, float t)
+    {
+        return ((1 - t) * a + t * b);
+    }
+    public static Vector2 Lerp(Vector2 a, Vector2 b, float t)
+    {
+        return ((1 - t) * a + t * b);
+    }
+    public static Vector3 Lerp(Vector3 a, Vector3 b, float t)
+    {
+        return ((1 - t) * a + t * b);
+    }
+}
