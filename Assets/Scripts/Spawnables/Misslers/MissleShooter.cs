ec93f3fda7049a8aac3be234ee6083ddbfcc4e6e
Aedan Ramos <aedanramos@MacBook-Air-3.local>
2024-02-01 12:50:59 -0600

    Missiler animations

--- Assets/Scripts/Spawnables/Misslers/MissleShooter.cs
+++ Assets/Scripts/Spawnables/Misslers/MissleShooter.cs
@@ -8,16 +8,31 @@ public class MissleShooter : MonoBehaviour
     public float shootInterval;
     public float engageDist;
     public int amt = 3;
+    public bool isShootin = false;
+    public NSpriteAnimation animationState;
+    public int startFrames;
+    public int endFrames;
+
 
     private float _timer;
+
     private void Start()
     {
+        animationState.SwapState("idle");
         if (target == null) target = GameObject.FindGameObjectWithTag("Player");
         _timer = shootInterval;
     }
     void Update()
     {
         _timer -= Time.deltaTime;
+
+        if (_timer < startFrames * animationState.timePerFrame && !isShootin){
+
+            animationState.SwapState("fire");
+            isShootin = true;
+
+        }
+
         if (_timer < 0)
         {
             _timer = shootInterval;
@@ -30,7 +45,15 @@ public class MissleShooter : MonoBehaviour
                     missile.GetComponent<CustomRigidbody2D>().AddForce(1000 * Random.insideUnitCircle);
                 }
             }
-            
+
         }
+
+      if (shootInterval-_timer > endFrames * animationState.timePerFrame && isShootin){
+            animationState.SwapState("idle");
+
+            isShootin = false;
+
+        }
+
     }
 }
