8aa6cc4b555155095e4e8db0e12c79f59e79e33a
Malcolm Roalson <mtroalson@gmail.com>
2024-11-28 21:06:58 -0600

    push ammo bar

--- Assets/Scripts/Static Info/GunInfo.cs
+++ Assets/Scripts/Static Info/GunInfo.cs
@@ -1,18 +1,19 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Static_Info
 {
     public class GunInfo : MonoBehaviour
     {
-        public static GunInfo GunInfoInstance => StaticInfoHolder.instance.GetCachedComponent<GunInfo>(); 
-        
-        public int clipCount;
-        public int clipCap;
+        public static GunInfo GunInfoInstance => StaticInfoHolder.instance.GetCachedComponent<GunInfo>();
+
+        public int ammoCount;
+        public float noShootRefillTime;
+        public float emptyRefillTime;
+        public float timeToRefillFully;
         public int bulletsPerShot;
         public int bulletsPerShotVarience;
         public float fireTime;
-        public float reloadTime;
-        public float refillTime;
         public float shotForce;
         public float forceVarience;
         public float lateralSeperation;
