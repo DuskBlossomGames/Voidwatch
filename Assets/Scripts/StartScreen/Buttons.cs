c109ee49bf4061fa90d1b7163deb57af0e33e865
Malcolm Roalson <mtroalson@gmail.com>
2025-04-30 11:46:04 -0500

    title screen, shop icons

--- Assets/Scripts/StartScreen/Buttons.cs
+++ Assets/Scripts/StartScreen/Buttons.cs
@@ -1,14 +1,48 @@
 using System.Collections;
 using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Util;
 using static Static_Info.PlayerData;
 
 public class Buttons : MonoBehaviour
 {
-    public void Play()
+    public ParticleSystem ps;
+    public AnimationCurve textFadeCurve, particleSpeed;
+    public float fadeTime, speedupTime, waitTime;
+
+    private TextMeshProUGUI[] _texts;
+    private Image[] _images;
+
+    private void Start()
     {
+        _texts = GetComponentsInChildren<TextMeshProUGUI>();
+        _images = GetComponentsInChildren<Image>();
+    }
+
+    public void Play() { StartCoroutine(PlayRoutine()); }
+    private IEnumerator PlayRoutine()
+    {
+        for (float t = 0; t < fadeTime; t += Time.fixedDeltaTime)
+        {
+            yield return new WaitForFixedUpdate();
+            foreach (var obj in _texts) obj.SetAlpha(textFadeCurve.Evaluate(1-t/fadeTime));
+            foreach (var obj in _images) obj.SetAlpha(1-t/fadeTime);
+        }
+
+        for (float t = 0; t < speedupTime; t += Time.fixedDeltaTime)
+        {
+            yield return new WaitForFixedUpdate();
+            var main = ps.main;
+            main.simulationSpeed = particleSpeed.Evaluate(t/speedupTime);
+        }
+
+        ps.gameObject.SetActive(false);
+        
+        yield return new WaitForSeconds(waitTime);
         SceneManager.LoadScene("LevelSelect");
     }
 
