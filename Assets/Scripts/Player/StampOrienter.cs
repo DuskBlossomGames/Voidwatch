ef863375d9fb51da9abd0b080069383c8420cb37
Malcolm Roalson <mtroalson@gmail.com>
2025-06-18 13:10:26 -0500

    stamp orienter

---
+++ Assets/Scripts/Player/StampOrienter.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class StampOrienter : MonoBehaviour
+    {
+        public float correctionPerSec;
+        
+        private Camera _cam;
+        
+        private void Start()
+        {
+            _cam = Camera.main!;
+            transform.rotation = _cam.transform.rotation;
+        }
+        
+        private void Update()
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, _cam.transform.rotation, correctionPerSec * Time.deltaTime);
+        }
+    }
+}
