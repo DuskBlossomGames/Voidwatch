5de3ae47640094d0be2d79c7032ac3ea5139ca47
Malcolm Roalson <mtroalson@gmail.com>
2025-06-21 15:32:19 -0500

    make player local to EA

--- Assets/Scripts/Spawnables/Damage/ElectricArea.cs
+++ Assets/Scripts/Spawnables/Damage/ElectricArea.cs
@@ -9,23 +9,21 @@ namespace Spawnables.Damage
         public float initialDamage;
         public float shieldDamagePerSecond;
         public float stunTime;
-
-        private Movement _player;
-
+        
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (!other.TryGetComponent(out _player) || _player.Stunned ||
+            if (!other.TryGetComponent<Movement>(out var player) || player.Stunned ||
                 !other.TryGetComponent<PlayerDamageable>(out var damageable)) return;
 
-            if (damageable.TakeEMP(initialDamage)) _player.Stun(stunTime);
+            if (damageable.TakeEMP(initialDamage)) player.Stun(stunTime);
         }
 
         private void OnTriggerStay2D(Collider2D other)
         {
-            if (!other.TryGetComponent(out _player) || _player.Stunned ||
+            if (!other.TryGetComponent<Movement>(out var player) || player.Stunned ||
                 !other.TryGetComponent<PlayerDamageable>(out var damageable)) return;
 
-            if (damageable.TakeEMP(shieldDamagePerSecond * Time.deltaTime)) _player.Stun(stunTime);
+            if (damageable.TakeEMP(shieldDamagePerSecond * Time.deltaTime)) player.Stun(stunTime);
         }
     }
 }
