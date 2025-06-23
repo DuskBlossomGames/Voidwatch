e19ab77b413cf50f0e4a8e4e5e2d92e82bdb8556
Malcolm Roalson <mtroalson@gmail.com>
2025-06-22 22:52:49 -0500

    small tweaks

--- Assets/Scripts/Spawnables/Damage/ElectricArea.cs
+++ Assets/Scripts/Spawnables/Damage/ElectricArea.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Timers;
 using Player;
 using UnityEngine;
+using Timer = Util.Timer;
 
 namespace Spawnables.Damage
 {
@@ -9,22 +11,33 @@ namespace Spawnables.Damage
         public float initialDamage;
         public float shieldDamagePerSecond;
         public float stunTime;
+        public float immunityTime;
 
+        private readonly Timer _immunity = new();
+        
         private void OnTriggerEnter2D(Collider2D other)
         {
-            if (!other.TryGetComponent<Movement>(out var player) || player.Stunned ||
+            if (!other.TryGetComponent<Movement>(out var player) || player.Stunned || !_immunity.IsFinished ||
                 !other.TryGetComponent<PlayerDamageable>(out var damageable)) return;
 
-            if (damageable.TakeEMP(initialDamage)) { player.Stun(stunTime); }
-            else { player.GetComponent<PlayerVFXController>().RunBif();}
+            if (damageable.TakeEMP(initialDamage))
+            {
+                _immunity.Value = immunityTime + stunTime;
+                player.Stun(stunTime);
+            }
+            else player.GetComponent<PlayerVFXController>().RunBif();
         }
 
         private void OnTriggerStay2D(Collider2D other)
         {
-            if (!other.TryGetComponent<Movement>(out var player) || player.Stunned ||
+            if (!other.TryGetComponent<Movement>(out var player) || player.Stunned || !_immunity.IsFinished ||
                 !other.TryGetComponent<PlayerDamageable>(out var damageable)) return;
 
-            if (damageable.TakeEMP(shieldDamagePerSecond * Time.deltaTime)) player.Stun(stunTime);
+            if (damageable.TakeEMP(shieldDamagePerSecond * Time.deltaTime))
+            {
+                _immunity.Value = immunityTime + stunTime;
+                player.Stun(stunTime);
+            }
         }
     }
 }
