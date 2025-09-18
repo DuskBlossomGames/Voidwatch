1f97cde335e675c95f33f2bbba643681f15bd31f
AeMcRa <aedanmramos@gmail.com>
2025-09-17 23:27:21 -0700

    SplashBypass

--- Assets/Scripts/SplashScreen/SplashListener.cs
+++ Assets/Scripts/SplashScreen/SplashListener.cs
@@ -1,14 +1,26 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
+using System.Collections;
 
 namespace SplashScreen
 {
     public class SplashListener : MonoBehaviour
     {
+      private IEnumerator splashcoroutine;
+
         private void Start()
         {
             GetComponent<VideoPlayer>().loopPointReached += _ => SceneManager.LoadScene("TitleScreen");
+
+            splashcoroutine = SplashScreenBypass(8.0f);
+            StartCoroutine(splashcoroutine);
+
+        }
+
+        private IEnumerator SplashScreenBypass(float bypassTime){
+          yield return new WaitForSeconds(bypassTime);
+          SceneManager.LoadScene("TitleScreen");
         }
     }
 }
